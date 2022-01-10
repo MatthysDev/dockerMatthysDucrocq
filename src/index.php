7dae49e7962a0a2c5862a347cab12eb3079793f7
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-01-10 16:10:47 +0100

    msg

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
+echo 'Hello';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 2 déc. 2021 à 16h31 </p>';
 phpinfo();
